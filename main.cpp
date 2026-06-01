4f4bf2a26b5602888fa16fe06b0a4b06ec3e279b
DHRUVRAJ SINGH <dhruvrajsingh737@gmail.com>
2026-06-01 18:27:27 +0000

    Write the neccessary code in main.cpp file for use.

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#include<iostream>
+#include <unordered_map>
+#include <vector>
+#include <string>
+#include <iomanip>
+#include <ctime>
+#include <algorithm>    
+
+using namespace std;
+
+
+unordered_map<string, double> exchangeRates = {
+    {"INR", 1.0},
+    {"USD", 83.5},
+    {"EUR", 91.2},
+    {"GBP", 107.0},
+    {"AED", 22.7},
+    {"JPY",  0.56}
+};
+int main(){}
